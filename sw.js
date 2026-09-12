132a6ce4524c58afdfcce2bc11ea793fb6ea2403
mariusbudiun-droid <mariusbudiun@gmail.com>
2026-09-12 21:50:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='part66-quiz-v15';
+const CACHE='part66-quiz-v16';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
